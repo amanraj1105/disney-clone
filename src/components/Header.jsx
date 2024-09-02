9761d52445e7e7485ffcf80334ced1de1e38f6e9
Aman Raj <aman1105raj@gmail.com>
2024-09-02 22:10:22 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import logo from '../assets/images/logo.png'
+import {HiHome,HiMagnifyingGlass,HiStar,HiPlayCircle,HiTv} from "react-icons/hi2"
+import { HiPlus,HiDotsVertical } from "react-icons/hi"
+import HeaderItem from './HeaderItem'
+function Header() {
+    const menu=[
+        {
+            name:'HOME',
+            icon:HiHome
+        },
+        {
+            name:'SEARCH',
+            icon:HiMagnifyingGlass
+        },
+        {
+            name:'WATCH LIST',
+            icon:HiPlus
+        },
+        {
+            name:'ORIGINALS',
+            icon:HiStar
+        },
+        {
+            name:'MOVIES',
+            icon:HiPlayCircle
+        },
+        {
+            name:'SERIES',
+            icon:HiTv
+        }
+
+    ]
+  return (
+    <div className= 'flex items-center justify-between p-5 w-screen'>
+        <div className='flex  gap-8 items-center'>
+        <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
+
+        <div className='hidden md:flex gap-8'>
+        {menu.map((item)=>(
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
+        ))}
+        </div>
+
+        <div className='flex md:hidden gap-8'>
+        {menu.map((item,index)=>index<3&&(
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
+        ))}
+        <div className='md:hidden'>
+            <HeaderItem name={''} Icon={HiDotsVertical}/>
+        </div>
+        </div>
+        </div>
+        
+        <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+        className='w-[40px] rounded-full'/>
+    </div>
+  )
+}
+export default Header
