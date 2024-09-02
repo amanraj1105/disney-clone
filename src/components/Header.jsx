dfb3ddb3fd7255d3d0be3c4d72428dcdcbb7a358
Aman Raj <aman1105raj@gmail.com>
2024-09-02 23:26:23 +0530

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/images/logo.png'
 import {HiHome,HiMagnifyingGlass,HiStar,HiPlayCircle,HiTv} from "react-icons/hi2"
 import { HiPlus,HiDotsVertical } from "react-icons/hi"
 import HeaderItem from './HeaderItem'
 function Header() {
+    const [toggle,setToggle]=useState(false);
     const menu=[
         {
             name:'HOME',
@@ -38,16 +39,23 @@ function Header() {
 
         <div className='hidden md:flex gap-8'>
         {menu.map((item)=>(
-            <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
+            <HeaderItem  name={item.name} Icon={item.icon}/>
         ))}
         </div>
 
-        <div className='flex md:hidden gap-8'>
+        <div className='flex md:hidden gap-5'>
         {menu.map((item,index)=>index<3&&(
-            <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
+            <HeaderItem  name={''} Icon={item.icon}/>
         ))}
-        <div className='md:hidden'>
-            <HeaderItem name={''} Icon={HiDotsVertical}/>
+        <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={''} Icon={HiDotsVertical}/> 
+
+           {toggle? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+            {menu.map((item,index)=>index>2&&(
+            <HeaderItem name={item.name} Icon={item.icon}/>
+        ))}
+        </div>:null}
+            
         </div>
         </div>
         </div>
