bc59ceb9a660daff75e78bf96060f7eac2f5db4e
Aman Raj <aman1105raj@gmail.com>
2024-09-05 01:20:53 +0530

    added Slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,68 +1,73 @@
-import React, { useState } from 'react'
-import logo from '../assets/images/logo.png'
-import {HiHome,HiMagnifyingGlass,HiStar,HiPlayCircle,HiTv} from "react-icons/hi2"
-import { HiPlus,HiDotsVertical } from "react-icons/hi"
-import HeaderItem from './HeaderItem'
+import React, { useState } from 'react';
+import logo from '../assets/Images/logo.png';
+import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
+import HeaderItem from './HeaderItem';
+
 function Header() {
-    const [toggle,setToggle]=useState(false);
-    const menu=[
+    const [toggle, setToggle] = useState(false);
+    const menu = [
         {
-            name:'HOME',
-            icon:HiHome
+            name: 'HOME',
+            icon: HiHome
         },
         {
-            name:'SEARCH',
-            icon:HiMagnifyingGlass
+            name: 'SEARCH',
+            icon: HiMagnifyingGlass
         },
         {
-            name:'WATCH LIST',
-            icon:HiPlus
+            name: 'WATCH LIST',
+            icon: HiPlus
         },
         {
-            name:'ORIGINALS',
-            icon:HiStar
+            name: 'ORIGINALS',
+            icon: HiStar
         },
         {
-            name:'MOVIES',
-            icon:HiPlayCircle
+            name: 'MOVIES',
+            icon: HiPlayCircle
         },
         {
-            name:'SERIES',
-            icon:HiTv
+            name: 'SERIES',
+            icon: HiTv
         }
+    ];
 
-    ]
-  return (
-    <div className= 'flex items-center justify-between p-5 w-screen'>
-        <div className='flex  gap-8 items-center'>
-        <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
+    return (
+        <div className='flex items-center justify-between p-5 w-screen'>
+            <div className='flex gap-8 items-center'>
+                <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
 
-        <div className='hidden md:flex gap-8'>
-        {menu.map((item)=>(
-            <HeaderItem  name={item.name} Icon={item.icon}/>
-        ))}
-        </div>
+                <div className='hidden md:flex gap-8'>
+                    {menu.map((item, index) => (
+                        <HeaderItem key={index} name={item.name} Icon={item.icon} />
+                    ))}
+                </div>
 
-        <div className='flex md:hidden gap-5'>
-        {menu.map((item,index)=>index<3&&(
-            <HeaderItem  name={''} Icon={item.icon}/>
-        ))}
-        <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
-            <HeaderItem name={''} Icon={HiDotsVertical}/> 
+                <div className='flex md:hidden gap-5'>
+                    {menu.slice(0, 3).map((item, index) => (
+                        <HeaderItem key={index} name={''} Icon={item.icon} />
+                    ))}
+                    <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+                        <HeaderItem key="toggle" name={''} Icon={HiDotsVertical} />
 
-           {toggle? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
-            {menu.map((item,index)=>index>2&&(
-            <HeaderItem name={item.name} Icon={item.icon}/>
-        ))}
-        </div>:null}
-            
+                        {toggle && (
+                            <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+                                {menu.slice(3).map((item, index) => (
+                                    <HeaderItem key={index} name={item.name} Icon={item.icon} />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            <img
+                src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+                className='w-[40px] rounded-full'
+            />
         </div>
-        </div>
-        </div>
-        
-        <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-        className='w-[40px] rounded-full'/>
-    </div>
-  )
+    );
 }
-export default Header
+
+export default Header;
